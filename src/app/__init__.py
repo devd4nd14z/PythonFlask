a00255e23b8697299d9e20e43cac8817a2080e62
d4nd14z <dev.dandiaz@gmail.com>
2023-02-02 17:37:21 -0500

    Se agregan nuevos archivos y configuraciones para uso de base de datos sqlite3 (Conexion a Base de datos con Flask-SQLAlchemy)

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,12 +2,15 @@
 
 from flask import Flask
 from config import Config
+from app.extensions import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
     # Initialize Flask extensions here
-    #
+    db.init_app(app)
+    
     # Register BluePrints here
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
